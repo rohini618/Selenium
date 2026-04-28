b09699e5ab3025b09ffd153ed1be0eea0e36c625
rohini618 <rohinibhagat618@gmail.com>
2026-04-28 11:35:27 +0530

    new demo file created

---
+++ src/test/java/Practice_HandlingMultipleElements/Demo1.java
@@ -0,0 +1,10 @@
+package Practice_HandlingMultipleElements;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+
+        System.out.println(a);
+    }
+}
